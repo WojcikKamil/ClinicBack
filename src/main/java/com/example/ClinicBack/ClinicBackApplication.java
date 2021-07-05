d9bcff51b074ba7d1e2f446977d11c35da608fcc
Kamil Wójcik <60044272+WojcikKamil@users.noreply.github.com>
2021-07-05 13:04:04 +0200

    Initial commit

---
+++ src/main/java/com/example/ClinicBack/ClinicBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClinicBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicBackApplication.class, args);
+	}
+
+}
